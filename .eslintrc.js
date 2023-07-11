aff8c814380cc8e288b95e8bcdadec1e61db3eb9
berkysr <brk.yasar@gmail.com>
2023-07-11 23:12:49 +0300

    chore: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,9 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint"
   ],
   overrides: [
     {
