15d6292abb641134b9b77f0e901d2dc926062d56
berkysr <brk.yasar@gmail.com>
2023-07-12 22:28:13 +0300

    feature: add i18n

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ThemeProvider, createTheme } from '@mui/system';
+import './i18n';
 
 const theme = createTheme({
   palette: {
